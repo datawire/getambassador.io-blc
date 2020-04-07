d0f33c91d36b7425b873c592521e64010cc425c0
Kenn Hussey <kennhussey@datawire.io>
2020-04-07 14:06:02 -0400

    Removing explicit enqueue for edgectl downloads

--- blc.js
+++ blc.js
@@ -99,7 +99,6 @@ function main(siteURL) {
 	if (!siteURL.endsWith('/')) {
 		siteURL += '/';
 	}
-	siteChecker.enqueue(siteURL+'reference/edgectl-download');
 };
 
 main(process.argv.slice(2)[0] || 'https://www.getambassador.io/');
